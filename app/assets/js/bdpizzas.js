e84bf58e69e450750c05b261d51de32afd21baee
eugenio <eugeniojhe@gmail.com>
2020-04-02 16:32:43 -0300

    Firt commit - development

---
+++ app/assets/js/bdpizzas.js
@@ -0,0 +1,41 @@
+let dataPizzaJson = [
+    {
+        id:1, name:'Mussarela', img:'',
+         prices:[40.50,38.70,35.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+         description:'Esta a descricao da primeira pizza'
+    },
+    {
+        id:2, name:'Calabreza',img:'', prices:[40.50,38.70,35.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        description:'Esta a descrição da Pizzzaa de Calabreza'
+    },
+
+    {
+        id:3, name:"CATUPIRI", img:'', prices:[37.80,36.00,35.00], size:['GRANDE','MEDIA','PEQUENA'],
+        description:'Esta é a descrição da Pizza de Catupiri', 
+    },
+
+    {
+        id:4, name:'MODA DA CASA', img:'', prices:[40.80, 39.00,37.00], sizes:['GRANDE','MEDIA','PEQUENS'],
+        description:'Esta é descrião da Pizza moda da casa'
+    },
+
+    {
+        id:5, name:'FRANGO E BACON', img:'',  prices:[45.00,42.00,40.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        description:'Descrião da pizza de FRANGO E BACON'
+    },
+    {
+        id:6, name:'MUSSARELA', img:'',  prices:[47.00,45.00,43.00], sizes:['GRAND','MEDIA','PEQUENA'],
+        description:'Esta é a descriçao da pizaa de MUSSARELA'
+    },
+    {
+        id:7, name:'PRESUNTO E QUEIJO', img:'', prices:[50.00,47.00,45.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        description:'Esta é a description da pizza acima'
+    },
+    {
+        id:8, name:'4 QUEIJS', img:'', prices:[36.00,35.00,34.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        description:'Descriçao da Pizza de 4 queijos'
+    }
+]
+
+
+
---
+++ app/assets/js/pizzas.js
@@ -0,0 +1,5 @@
+console.log(`Numero de linhas de base de dados ===>${dataPizzaJson.length}`);
+
+dataPizzaJson.map((data,index)=>{
+    console.log(document.querySelector('body .data-pizzas .pizza-items')); 
+});
