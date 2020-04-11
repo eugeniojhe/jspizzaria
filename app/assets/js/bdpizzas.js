352b920ee978a47e93a8974a1806626698bbdb2a
eugenio <eugeniojhe@gmail.com>
2020-04-11 11:02:57 -0300

    keeping development

--- app/assets/js/bdpizzas.js
+++ app/assets/js/bdpizzas.js
@@ -1,38 +1,38 @@
 let dataPizzaJson = [
     {
-        id:1, name:'Mussarela', img:'',
-         prices:[40.50,38.70,35.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        id:1, name:'Mussarela', img:'app/assets/media/images/pizza.png',
+         prices:[40.50,38.70,35.00], weights:['100g', '530g', '860g'],sizes:['GRANDE','MEDIA','PEQUENA'],
          description:'Esta a descricao da primeira pizza'
     },
     {
-        id:2, name:'Calabreza',img:'', prices:[40.50,38.70,35.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        id:2, name:'Calabreza', img:'app/assets/media/images/pizza2.png', prices:[40.50,38.70,35.00], weights:['100g', '530g', '860g'],sizes:['GRANDE','MEDIA','PEQUENA'],
         description:'Esta a descrição da Pizzzaa de Calabreza'
     },
 
     {
-        id:3, name:"CATUPIRI", img:'', prices:[37.80,36.00,35.00], size:['GRANDE','MEDIA','PEQUENA'],
-        description:'Esta é a descrição da Pizza de Catupiri', 
+        id:3, name:"CATUPIRI", img:'app/assets/media/images/pizza3.png', prices:[37.80,36.00,35.00], weights:['100g', '530g', '860g'], size:['GRANDE','MEDIA','PEQUENA'],
+        description:'Esta é a descrição da Pizza de Catupiri pizza realmente muito boa feita com os mais puros engredientes,que irão agradar a todos'
     },
 
     {
-        id:4, name:'MODA DA CASA', img:'', prices:[40.80, 39.00,37.00], sizes:['GRANDE','MEDIA','PEQUENS'],
+        id:4, name:'MODA DA CASA', img:'app/assets/media/images/pizza4.png', prices:[40.80, 39.00,37.00], weights:['100g', '530g', '860g'], sizes:['GRANDE','MEDIA','PEQUENS'],
         description:'Esta é descrião da Pizza moda da casa'
     },
 
     {
-        id:5, name:'FRANGO E BACON', img:'',  prices:[45.00,42.00,40.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        id:5, name:'FRANGO E BACON', img:'app/assets/media/images/pizza5.png',  prices:[45.00,42.00,40.00], weights:['100g', '530g', '860g'], sizes:['GRANDE','MEDIA','PEQUENA'],
         description:'Descrião da pizza de FRANGO E BACON'
     },
     {
-        id:6, name:'MUSSARELA', img:'',  prices:[47.00,45.00,43.00], sizes:['GRAND','MEDIA','PEQUENA'],
+        id:6, name:'MUSSARELA', img:'app/assets/media/images/pizza6.png',  prices:[47.00,45.00,43.00], weights:['100g', '530g', '860g'], sizes:['GRAND','MEDIA','PEQUENA'],
         description:'Esta é a descriçao da pizaa de MUSSARELA'
     },
     {
-        id:7, name:'PRESUNTO E QUEIJO', img:'', prices:[50.00,47.00,45.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        id:7, name:'PRESUNTO E QUEIJO', img:'app/assets/media/images/pizza7.png', prices:[50.00,47.00,45.00], weights:['100g', '530g', '860g'],sizes:['GRANDE','MEDIA','PEQUENA'],
         description:'Esta é a description da pizza acima'
     },
     {
-        id:8, name:'4 QUEIJS', img:'', prices:[36.00,35.00,34.00], sizes:['GRANDE','MEDIA','PEQUENA'],
+        id:8, name:'4 QUEIJS', img:'app/assets/media/images/pizza.png', prices:[36.00,35.00,34.00], weights:['100g', '530g', '860g'], sizes:['GRANDE','MEDIA','PEQUENA'],
         description:'Descriçao da Pizza de 4 queijos'
     }
 ]
