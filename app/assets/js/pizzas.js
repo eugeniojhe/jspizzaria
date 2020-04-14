50471d2cf661152cc2c605cd26233a7df9768fae
eugenio <eugeniojhe@gmail.com>
2020-04-14 18:52:19 -0300

    keeps development

--- app/assets/js/pizzas.js
+++ app/assets/js/pizzas.js
@@ -3,7 +3,11 @@
 
 */
 
-
+//Variables 
+let qtOrderedPizza; //
+let cart= [];
+let itemPrices = []; 
+let itemKey; 
 
 /*const f = (el)=> {
     return document.querySelector(el); 
@@ -20,7 +24,9 @@ const cm = ()=>{
 //Products functions 
 const qs = (el)=>document.querySelector(el); 
 const qsa = (el)=>document.querySelectorAll(el);
-let qtOrderedPizza; 
+
+
+ 
 //Products lists  
 dataPizzaJson.map((data,index)=>{
     //let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
@@ -36,6 +42,10 @@ dataPizzaJson.map((data,index)=>{
         ev.preventDefault();
         qtOrderedPizza = 1; 
         let key = ev.target.closest('.pizza-item').getAttribute('data-key');
+        itemKey = data.id;
+        itemPrices = data.prices;
+            
+        
         //Comment: Line commented about is another way 
         //         by putting product name into html 
        // qs('.pizzaInfo h1').innerHTML = dataPizzaJson[key].name; 
@@ -88,8 +98,51 @@ qsa('.pizzaInfo--size').forEach((weight,index)=>
      weight.addEventListener('click',(e)=>
      {
          qs('.pizzaInfo--size.selected').classList.remove('selected'); 
-         e.target.classList.add('selected');
-     });   
+         weight.classList.add('selected');
+      });   
 });
+
+qs('.pizzaInfo--addButton').addEventListener('click',(e)=>{
+    let size = parseInt(qs('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let whatPizza =itemKey+"@"+size; 
+    let ret = cart.findIndex((item)=>{
+        return item.whatPizza == whatPizza; 
+    });
+
+    if (ret > -1){
+        cart[ret].qt += qtOrderedPizza;  
+    }else {
+        cart.push({
+            whatPizza:whatPizza, 
+            id:itemKey,
+            size:size,
+            qt:qtOrderedPizza,
+            price: itemPrices[size]
+        });
+    }
+     updateCart(); 
+     cm();    
+});
+
+//Update the cart 
+const updateCart = ()=>
+{
+    if (cart.length > 0){
+        let subTotal = 0;
+        let discount = 0;
+        let total = 0; 
+        qs("aside").classList.add('show');
+        for(let c in cart){
+            let pizzaItem = dataPizzaJson.find((dataPizza)=>{
+                return dataPizza.id == cart[i].id; 
+                subtotal += cart[id].price * cart[i].qt; 
+            }) 
+        }
+    }else{
+        qs("aside").classList.remove('show'); 
+    }
+}
+
+
 
 
